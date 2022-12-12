dabbff1df3a9a79c4667d4610e46a21fca0d63d0
JiaCheng-code <675232630@qq.com>
2022-12-12 23:39:22 +0800

    style(LoginPage):登录ui完成

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import 'normalize.css/normalize.css';
 
 import {Provider} from "react-redux/es/exports";
-import store from './store/store'
+import store from './store/store';
+import "./globe.scss"
 
 
 const root = ReactDOM.createRoot(
--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,9 +1,25 @@
 import React, {FC} from "react";
+import styles from './styles.module.scss'
+import classNames from "classnames";
 
-const LoginPage:FC = ()=>{
+import logoImage from '../../resources/logo.png'
+
+const LoginPage: FC = () => {
     return (
-        <div>
-            欢迎来到登录页
+        <div className={classNames(styles.loginPage, 'background_main_color')}>
+            <div className={classNames(styles.loginBox,'background_secondary_color')}>
+                {/*logo*/}
+                <div className={styles.logoContainer}>
+                    <img className={styles.logoImage} src={logoImage} alt=""/>
+                </div>
+                {/*title*/}
+                <div className={classNames(styles.titleContainer,'text_main_color')}>
+                    <h2>欢迎来到登录页面</h2>
+                </div>
+                {/*input btn*/}
+            </div>
+
+
         </div>
     )
 }
